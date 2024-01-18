4a0c8e72d35286841e4f700857631e1bb486dbb8
KUNCHE-VEERA-VENKATA-SAI-KARTHIK <kunchesaikarthik999@gmail.com>
2024-01-18 14:52:54 +0530

    componets built

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -4,11 +4,48 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [color, setcolor] = useState("black");
+  const [color, setColor] = useState("white");
+
+  function colorChange(c) {
+    setColor(c);
+    console.log(typeof c);
+  }
 
   return (
     <>
-      <h1 className="text-3xl bg-red-950 ">Hello world!</h1>
+      <div
+        className="w-full h-screen flex align-bottom "
+        style={{ backgroundColor: color }}
+      >
+        <h1 className="underline  flex  justify-center align-bottom ">hi</h1>
+        <div className="bg-white  flex flex-wrap   px-3 py-5  ">
+          <button
+            className="bg-red rounded-xl outline-dashed px-3 py-5"
+            onClick={() => {
+              colorChange("red");
+            }}
+          >
+            red{" "}
+          </button>
+          <button
+            className="bg-red rounded-xl   outline-dashed px-3 py-3 "
+            onClick={() => {
+              colorChange("blue");
+            }}
+          >
+            {" "}
+            blue{" "}
+          </button>
+          <button
+            className="bg-red rounded-xl flex  outline-dashed px-3"
+            onClick={() => {
+              colorChange("green");
+            }}
+          >
+            green
+          </button>
+        </div>
+      </div>
     </>
   );
 }
